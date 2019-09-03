b5ea906fb5959009074044070b655150275e0b17
liufei <649912343@qq.com>
2019-09-03 18:02:35 +0800

    add eject test createContext

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{createContext,useState} from 'react';
+const BatteryContext=createContext()
+const OnlineContext=createContext(false)
 
+const Middle=(props)=>{
+	return (
+		<Leaf/>
+	)
+}
+const Leaf=()=>{
+	return (
+		<BatteryContext.Consumer>
+			
+				{
+					battery=>(
+					<OnlineContext.Consumer>
+						{online=><h1>battery:{battery},online: {String(online)}</h1>}
+					</OnlineContext.Consumer>
+					)
+				
+				}
+			
+		</BatteryContext.Consumer>
+	)
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [battery,setBattery]=useState(60)
+	const [isOnline,setIsOnline]=useState(false)
+
+	return (
+		<>
+			<button onClick={()=>setBattery(battery+1)}>充电</button>
+			<button onClick={()=>setIsOnline(!isOnline)}>切换状态</button>
+
+			<BatteryContext.Provider value={battery}>
+				<OnlineContext.Provider value={isOnline}>
+					<Middle/>
+				</OnlineContext.Provider>
+			</BatteryContext.Provider>
+		</>
+	);
 }
 
 export default App;
