c0d15d7c21582b7f80951e56388a471db38d0f47
liufei <649912343@qq.com>
2019-09-11 18:03:41 +0800

    5-3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React,{useState,useEffect,useCallback,useRef} from 'react';
+import React,{useState,useEffect,useCallback,useRef,memo} from 'react';
+import * as createAction from './action'
 import './index.css'
-var idSeq=0
-function Control(props){
+
+function bindActionCreateors(actionCreateors,dispatch){
+    let ret={}
+    //循环绑定函数时候要用到let或者闭包 不然最后key为最后的值
+    for(let key in actionCreateors){
+        ret[key]=function(...args){
+            
+            const actionCreateor=actionCreateors[key]
+            const action=actionCreateor(...args)
+            dispatch(action)
+        }
+    }
+    return ret
+}
+const Control=memo(function Control(props){
+    console.log('control render')
     const {addTodo}=props
     var inputRef=useRef()
     const onSubmit=(e)=>{
@@ -9,7 +24,7 @@ function Control(props){
         const newText=inputRef.current.value.trim()
         if(newText.length===0){return}
         addTodo({
-            id:idSeq++,
+            id:Date.now(),
             text:newText,
             complete:false
         })
@@ -26,47 +41,94 @@ function Control(props){
                 />
         </form>
     </div>
-}
+})
 
-function TodoItem(props){
-    
-}
-function Todos(props){
-    const {todos,romoveTodo,toggleTodo}=props;
+const TodoItem=memo(function TodoItem(props){
+    console.log('TodoItem render')
+    console.log(props.removeTodo.toString())
+    const {
+        todo:{
+            id,
+            text,
+            complete
+        },
+    }=props
+    return (
+        <li className='todo-item'>
+            <input type="checkbox" onChange={()=>props.toggleTodo(id)} checked={complete}/>
+            <label className={complete?'complete':''}>{text}</label>
+            <button onClick={()=>props.removeTodo(id)}>&#xd7;</button>
+        </li>
+    )
+})
+
+const Todos=memo(function Todos(props){
+    console.log('Todos render')
+    const {todos,removeTodo,toggleTodo}=props;
 
     return (
         <ul>
             {
-                todos.map(todo=>(
-                    <li key={todo.id}></li>
+                todos.map((todo,index)=>(
+                    <TodoItem key={index} todo={todo} removeTodo={removeTodo} toggleTodo={toggleTodo}></TodoItem>
                 ))
             }
         </ul>
     )
-}
+})
+const LS_KEY='_$todo_'
 function TodoList (){
+    console.log('TodoList render')
     const [todos,setTodos]=useState([])
-    const addTodo=useCallback((todo)=>{
-        setTodos(todos=>[...todos,todo])
+    
+    const dispatch=useCallback((action)=>{
+        const {type,payload}=action
+        switch(type){
+            case 'set':
+                setTodos(payload)
+                break;
+            case 'add':
+                setTodos(todos=>[...todos,payload])
+                break;
+            case 'remove':
+                setTodos(todos=>todos.filter(todo=>todo.id!==payload))
+                break;
+            case 'toggle':
+                setTodos(todos=>todos.map(todo=>{
+                        return todo.id===payload?
+                        {
+                            ...todo,
+                            complete:!todo.complete
+                        }
+                        :todo
+                }))
+                break;
+            default:
+        }
     },[])
-    const romoveTodo=useCallback((id)=>{
-        setTodos(todos=>todos.filter(todo=>todo.id!==id))
+    useEffect(()=>{
+        const todos=JSON.parse(localStorage.getItem(LS_KEY))||[]
+        dispatch({type:'set',payload:todos})
     },[])
-    const toggleTodo=useCallback((id)=>{
-        setTodos(todos=>todos.map(todo=>{
-            return todo.id===id?
-            {
-                ...todo,
-                complete:!todo.complete
-            }
-            :todo
-        }))
-    },[])
+    useEffect(()=>{
+        localStorage.setItem(LS_KEY,JSON.stringify(todos))
+    },[todos])
     return (
         <div className='todo-list'>
-            <Control addTodo={addTodo}/>
-            <Todos toggleTodo={toggleTodo} romoveTodo={romoveTodo} todos={todos}/>
+            <Control 
+                {...useCallback(bindActionCreateors({
+                    addTodo:createAction.createAdd
+                },dispatch),[])}
+            />
+            <Todos 
+                {...useCallback(bindActionCreateors({
+                    toggleTodo:createAction.createToggle,
+                    removeTodo:createAction.createRemove,
+                },dispatch),[])}
+            todos={todos}/>
         </div>
     )
 }
+
+
 export default TodoList
